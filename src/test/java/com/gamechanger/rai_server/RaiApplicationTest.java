8e5a8499fdcbf2867cdaa852282afe646f7104fe
mayursamnani36 <mayursamnani36@gmail.com>
2024-01-16 23:03:43 +0530

    Added tests for service implementation and request validator

---
+++ src/test/java/com/gamechanger/rai_server/RaiApplicationTest.java
@@ -0,0 +1,13 @@
+package com.gamechanger.rai_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = RaiApplication.class)
+public class RaiApplicationTest {
+
+    @Test
+    public void contextLoads() {
+        // This test method is just to ensure that the application context loads without errors
+    }
+}
